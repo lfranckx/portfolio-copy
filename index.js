426df7d7449bc03785a381b6c565a5ec7cbc12cc
Bowie <bowie@thinkful.com>
2019-10-06 18:36:29 -0700

    update portfolio

--- index.js
+++ index.js
@@ -6,5 +6,14 @@ function jumpPage() {
     });
 }
 
+function dropMenuJump() {
+    $('.dropMenu li').click(function() {
+        let id = "#" + $(this).attr('goto');
+        let top = $(id).position().top;
+        $('html').scrollTop(top);
+    });
+}
+
+dropMenuJump();
 jumpPage();
 // openNewTab();
